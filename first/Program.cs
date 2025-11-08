18bdd4f69727d6925924422d92c798673e6265dd
pawelsurmiak <pawel.surmiak@webcrafters.studio>
2025-11-08 10:01:39 +0100

    dodanie pilotów XD

--- first/Program.cs
+++ first/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    
+    
+
+    public static void Main(string[] args)
+    {
+        // int dateOfBirth;
+        // string name;
+        
+        // Console.WriteLine("Podaj imie: ");
+        // name = Console.ReadLine();
+        
+        // Console.WriteLine("Podaj rok urodzenia: ");
+        // dateOfBirth = int.Parse(Console.ReadLine());
+        
+        // int age = 2025 - dateOfBirth;
+        // Console.WriteLine($"Witaj {name}! Masz {age} lat.");
+        // if(age>=18){
+        //     Console.WriteLine("Jestes pelnoletni.");
+        // } else {
+        //     Console.WriteLine("Nie jestes pelnoletni.");
+        // }
+        int height;
+        
+        Console.WriteLine("witam pilotów linii lotniczych lot");
+        
+        
+        Console.WriteLine("Jak wysoko lecisz?");
+        height = int.Parse(Console.ReadLine());
+        if(height > 1000)
+        {
+            Console.WriteLine("Lecisz za wysoko");
+        }
+        else if(height >= 800 && height <= 1000)
+        {
+            Console.WriteLine("Lecisz dobrze");
+        }
+        else
+        {
+            Console.WriteLine("Lecisz za nisko");
+        }
+    }
+}
--- first/obj/Debug/net9.0/first.AssemblyInfo.cs
+++ first/obj/Debug/net9.0/first.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c61dd61008f4ebdc55028d2d3fd4176cff5c58b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
