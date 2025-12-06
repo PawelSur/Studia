6ebb0fe7a94e2e09d007bd5acf29e9225e20bc85
pawelsurmiak <pawel.surmiak@webcrafters.studio>
2025-12-06 08:55:49 +0100

    cwel

---
+++ primary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Liczby pierwsze od 2 do " + n + ":");
+
+        for (int i = 2; i <= n; i++)
+        {
+            bool isPrime = true;
+
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
