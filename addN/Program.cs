fd18365e3e4c60096789da303e5b2be0de8ba402
pawelsurmiak <pawel.surmiak@webcrafters.studio>
2025-12-06 08:19:33 +0100

    dodanie tematu3

---
+++ addN/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    
+    
+
+    public static void Main(string[] args)
+    {   
+        int suma = 0;
+        int n = 0;
+        Console.WriteLine("Podaj rozmiar tablicy: ");
+        n = int.Parse(Console.ReadLine());
+        for(int i=1; i<=n; i++){
+            suma += i;
+        }
+        Console.WriteLine($"Suma elementów tablicy to: {suma}");
+        
+    }
+}
---
+++ evenOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    
+    
+
+    public static void Main(string[] args)
+    {   
+        int num =1 ;
+        while(num <=50){
+            if(num % 2 ==0){
+                Console.WriteLine($"{num} - parzysta");
+            } else {
+                Console.WriteLine($"{num} - nieparzysta");
+            }
+            num++;
+        }
+    }
+}
