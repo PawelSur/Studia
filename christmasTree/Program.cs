22117218a64802837659053d6f7f57b1a6537f1c
pawelsurmiak <pawel.surmiak@webcrafters.studio>
2025-12-06 09:21:19 +0100

    adam to cwel

---
+++ christmasTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n ;
+        Console.Write("Podaj wysokość piramidy: ");
+        n = int.Parse(Console.ReadLine());  
+        for (int i = 0; i < n; i++)
+        {
+            for(int j= 0; j < n - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++)
+            {
+                Console.Write("*");     
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ guessGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       Random rand = new Random();
+         int numberToGuess = rand.Next(1, 101);
+        int userGuess = 0;
+        do{
+            Console.Write("Zgadnij liczbę od 1 do 100: ");
+            userGuess = int.Parse(Console.ReadLine());
+            if(userGuess < numberToGuess){
+                Console.WriteLine("Za nisko!");
+            } else if(userGuess > numberToGuess){
+                Console.WriteLine("Za wysoko!");
+            } else {
+                Console.WriteLine("Gratulacje! Zgadłeś liczbę!");
+            }
+        }while(userGuess != numberToGuess);
+    }
+}
