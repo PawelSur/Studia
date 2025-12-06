eebd249dabb2e8f96772b9b577412c5fa486f5a0
pawelsurmiak <pawel.surmiak@webcrafters.studio>
2025-12-06 09:05:55 +0100

    cwel

---
+++ fibbo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+
+        Console.WriteLine("Ciąg Fibonacciego:");
+
+        for (int i = 0; i <= n; i++)
+        {
+            Console.Write(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
